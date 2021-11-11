a9c3b6da6514892203f4bdbc9e81e3771a7beb02
Lua Zhi Zhan <zhizhanlua@gmail.com>
2021-11-11 21:53:07 +0800

    Remove helper.c

--- testing.c
+++ testing.c
@@ -63,3 +63,10 @@ float testing(float data[ROW][COLUMN], trainingResults trainResult,
 
     return probOfError;
 }
+
+// Gaussain distribution using variance formula
+float gaussianDistribution(float value, float variance, float mean)
+{
+    float fx = 1 / sqrt(2 * M_PI) * exp(-0.5 * pow((value - mean), 2) / variance);
+    return fx;
+}
--- testing/sitting.c
+++ testing/sitting.c
@@ -1,3 +1,4 @@
+float gaussianDistribution(float value, float variance, float mean);
 void testSitting(float sitHrs, sitting probs, float *normalResult, float *alteredResult)
 {
     *normalResult *= gaussianDistribution(sitHrs, probs.normalHrsVariance,
